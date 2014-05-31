243ce03ff211241a42eaf15a784700eb6af1e2b6
stassavickii <ooofff@mail.ru>
2014-06-01 01:13:52 +0400

    danthes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Bannermaker::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.after_initialize do
+    #!/bin/bash
+    system 'rackup danthes.ru -s thin -E production'
+  end
 end
